7226c7c577be9d3412e6a6ee20fe5d5310afb62a
Ms2ger <Ms2ger@gmail.com>
2021-09-17 16:30:19 +0200

    Add tests for TimeZone.from().

--- polyfill/test/TimeZone/constructor/from/argument-object.js
+++ polyfill/test/TimeZone/constructor/from/argument-object.js
@@ -13,6 +13,7 @@ const objects = [
   new CustomTimeZone("Africa/Accra"),
   {},
   { getPlainDateTimeFor: null },
+  { id: 'Etc/Custom' },
 ];
 
 const thisValues = [
@@ -29,4 +30,18 @@ for (const thisValue of thisValues) {
     const result = Temporal.TimeZone.from.call(thisValue, object);
     assert.sameValue(result, object);
   }
+
+  const zdt = new Temporal.ZonedDateTime(0n, 'Africa/Cairo');
+  const fromZdt = Temporal.TimeZone.from.call(thisValue, zdt);
+  assert.sameValue(fromZdt, zdt.timeZone);
+  assert.sameValue(fromZdt.id, 'Africa/Cairo');
+
+  const tz = new Temporal.TimeZone('Africa/Cairo');
+  const fromPropertyBagObject = Temporal.TimeZone.from.call(thisValue, { timeZone: tz });
+  assert.sameValue(fromPropertyBagObject, tz);
+  assert.sameValue(fromPropertyBagObject.id, 'Africa/Cairo');
+
+  const fromPropertyBagString = Temporal.TimeZone.from.call(thisValue, { timeZone: 'Africa/Cairo' });
+  assert(fromPropertyBagString instanceof Temporal.TimeZone);
+  assert.sameValue(fromPropertyBagString.id, 'Africa/Cairo');
 }
